11d302622a5fa2a32e47cbf64ea1d8d0213c9086
codethelabs <codethelabs@gmail.com>
2025-08-26 20:04:13 +0300

    more changes

--- lib/widgets/reply_attachment_preview.dart
+++ lib/widgets/reply_attachment_preview.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ReplyAttachmentPreview extends StatefulWidget {
   final Map<String, dynamic> attachment;
@@ -59,9 +60,17 @@ class _ReplyAttachmentPreviewState extends State<ReplyAttachmentPreview> {
 
     if (extension.startsWith('video')) {
       if (_isLoading) {
-        preview = const CircularProgressIndicator(strokeWidth: 2);
+        preview = Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: CircularProgressIndicator(strokeWidth: 1.0, color: Colors.teal, valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 110, 110, 110))),
+        );
       } else if (_thumbnailData != null) {
-        preview = Image.memory(_thumbnailData!, fit: BoxFit.fill);
+        preview = Image.memory(
+          _thumbnailData!,
+          fit: BoxFit.cover,
+          width: 60, // Set width to maintain 4:3 ratio with height 60
+          height: 60,
+        );
       } else {
         preview = const Icon(Icons.videocam, size: 24, color: Colors.white);
       }
@@ -70,32 +79,50 @@ class _ReplyAttachmentPreviewState extends State<ReplyAttachmentPreview> {
         case 'image/jpeg':
         case 'image/png':
         case 'image':
-          preview = Image(
-            image: isLocalFile
-                ? FileImage(File(widget.attachment['url']))
-                : NetworkImage(widget.attachment['url']) as ImageProvider,
-            fit: BoxFit.fill,
-          );
+          preview = isLocalFile
+              ? Image.file(
+                  File(widget.attachment['url']),
+                  fit: BoxFit.cover,
+                  width: 50,
+                  height: 60,
+                )
+              : CachedNetworkImage(
+                  imageUrl: widget.attachment['url'],
+                  fit: BoxFit.cover,
+                  width: 50,
+                  height: 60,
+                  placeholder: (context, url) => const SizedBox(
+                    height: 10,
+                    width: 10,
+                    child: CircularProgressIndicator(strokeWidth: 1.0, color: Colors.teal, valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 110, 110, 110))),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(
+                    Icons.broken_image,
+                    size: 24,
+                    color: Colors.white,
+                  ),
+                );
           break;
         case 'audio/mp3':
         case 'voice':
           preview = const Icon(Icons.audiotrack, size: 24, color: Colors.white);
           break;
         case 'application/pdf':
-          preview =
-              const Icon(Icons.picture_as_pdf, size: 24, color: Colors.white);
+          preview = const Icon(Icons.picture_as_pdf, size: 24, color: Colors.white);
           break;
         default:
-          preview =
-              const Icon(Icons.insert_drive_file, size: 24, color: Colors.white);
+          preview = const Icon(Icons.insert_drive_file, size: 24, color: Colors.white);
       }
     }
 
-    return SizedBox(
-        width: 80,
-        height: 60,
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: preview));
+    return Container(
+      width: 60,
+      height: 60, // 4:3 aspect ratio (80:60)
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: preview,
+    );
   }
 }
